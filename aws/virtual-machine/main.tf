3aca911e3496e946f31b21dfa613d9f2e6731abb
Igor Kravets <igrkio@gmail.com>
2019-09-19 15:01:38 +0300

    Update main.tf

--- aws/virtual-machine/main.tf
+++ aws/virtual-machine/main.tf
@@ -19,7 +19,7 @@ data "aws_ami" "ubuntu" {
 }
 
 
-resource "aws_instance" "terraform-test-instance" {
+resource "aws_instance" "terraform-itest-instance" {
   ami             = "${data.aws_ami.ubuntu.id}"
   instance_type   = "${var.instance_type}"
 
