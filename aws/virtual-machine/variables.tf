6f5d7f2bb814a1f52efdb327ed1a7b6f63fce3b7
Igor Kravets <igrkio@gmail.com>
2019-09-26 11:30:42 +0300

    Update variables.tf

--- aws/virtual-machine/variables.tf
+++ aws/virtual-machine/variables.tf
@@ -5,3 +5,12 @@ variable "region" {
 variable "instance_type" {
   default = "m1.small"
 }
+
+variable "scalr_aws_access_key" {
+  default = ""
+}
+
+
+variable "scalr_aws_secret_key" {
+  default = ""
+}
